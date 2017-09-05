d0f52d66389aee39c0f79aed9934475aeaa48b88
Simon <immrss@sina.com>
2017-09-06 00:29:58 +0800

    add imageBrowser

--- src/ImageBrowser.js
+++ src/ImageBrowser.js
@@ -1,36 +1,78 @@
 import React, { PureComponent } from 'react'
-import { View,Text,Image,Button,Alert,Modal,ScrollView,Dimensions } from 'react-native'
+import { 
+  View,
+  Text,
+  Image,
+  Alert,
+  Modal,
+  ScrollView,
+  Dimensions,
+  TouchableWithoutFeedback
+} from 'react-native'
 
 let screenW = Dimensions.get('screen').width;
 let screenH = Dimensions.get('screen').height;
 
 export default class ImageBrowser extends PureComponent {
-  render() {
-    if (!this.props.images.length) {
+
+  constructor() {
+    super();
+    this.state = {
+      modalVisible: false,
+      images: [],
+      initialIndex: 0
+    }
+  }
+
+  show = (images,index)=>{
+    this.setState({
+      modalVisible: true,
+      images: images,
+      initialIndex: index
+    });
+  }
+
+  _dismiss = ()=>{
+    this.setState({
+      modalVisible: false
+    });
+  }
+
+  render = ()=>{
+    if (!this.state.images.length) {
       return null;
     }
     return (
       <Modal
-        visible={this.props.modalVisible}
+        visible={this.state.modalVisible}
         style={{backgroundColor:'black'}}
       >
         <ScrollView
+          ref={(scrollView) => {
+            if (scrollView) {
+              scrollView.scrollTo({x:screenW*this.state.initialIndex,y:0,animated:false});
+            }
+          }}
           style={{flex:1,backgroundColor:'black'}}
           showsHorizontalScrollIndicator = {false}
           horizontal={true}
           pagingEnabled={true}
-          alwaysBounceHorizontal={this.props.images.length>1}
+          alwaysBounceHorizontal={this.state.images.length>1}
         >
           {
-            this.props.images.map((image,i)=>{
-              let r = (i==this.props.images.length-1) ? 0 : 10;
+            this.state.images.map((image,i)=>{
+              let r = (i==this.state.images.length-1) ? 0 : 10;
               return (
-                <Image 
-                  resizeMode='center'
-                  key={i} 
-                  style={{width:screenW,height:screenH,marginRight:r}} 
-                  source={{uri:image.picUrl}} 
-                />
+                <TouchableWithoutFeedback
+                  key={i}
+                  onPress={this._dismiss} 
+                >
+                  <Image 
+                    resizeMode='contain'
+                    style={{width:screenW,height:screenH}} 
+                    source={{uri:image.picUrl}} 
+                  />
+                </TouchableWithoutFeedback>
               )
             })
           }
